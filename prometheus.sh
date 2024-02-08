103716b9219cab207d4b8021a4ad201dd41b4527
Aditya Goel <ubuntu@ip-172-31-8-133.us-east-2.compute.internal>
2024-02-08 16:35:39 +0000

    Added prometheus installation steps

---
+++ prometheus.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+#Create necessary directories and users
+sudo apt update -y
+sudo useradd --no-create-home --shell /bin/false prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+sudo chown prometheus:prometheus /var/lib/prometheus
+cd /tmp/
+
+
+#Download and install
+wget https://github.com/prometheus/prometheus/releases/download/v2.46.0/prometheus-2.46.0.linux-amd64.tar.gz
+tar -xvf prometheus-2.46.0.linux-amd64.tar.gz
+
+
+#Move files and folders to appropriate locations
+cd prometheus-2.46.0.linux-amd64
+sudo mv console* /etc/prometheus
+sudo mv prometheus.yml /etc/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus
+sudo mv prometheus /usr/local/bin/
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+
+
+#Creating a Prometheus systemd file
+cd /etc/systemd/system/
+sudo touch prometheus.service
+sudo tee -a prometheus.service > /dev/null <<EOT
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+
+
+#Start the service
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
+sudo systemctl status prometheus
