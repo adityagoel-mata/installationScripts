feb2f17f086d8eb849cb3305f4c4a55267e7252e
adityagoel-mata <50823187+adityagoel-mata@users.noreply.github.com>
2024-03-01 14:16:09 -0500

    Added aws-cli installation script

---
+++ aws-cli.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get install unzip
+unzip awscliv2.zip
+sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin --update
+aws –version
